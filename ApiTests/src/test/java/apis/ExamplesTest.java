bb394040dc4dd74844cf3afd216943aaad36b2bf
Parul Bhasin <parul.bhasin@volvocars.com>
2022-07-25 16:01:47 -0700

    more folders

--- ApiTests/src/test/java/apis/ExamplesTest.java
+++ ApiTests/src/test/java/apis/ExamplesTest.java
@@ -1,15 +1,17 @@
-package examples;
+package apis;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
+
 
 class ExamplesTest {
 
     @Test
     void testParallel() {
-        Results results = Runner.path("classpath:examples")
+        Results results = Runner.path("classpath:apis")
                 //.outputCucumberJson(true)
                 .parallel(5);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
--- ApiTests/src/test/java/apis/users/UsersRunner.java
+++ ApiTests/src/test/java/apis/users/UsersRunner.java
@@ -1,4 +1,4 @@
-package examples.users;
+package apis.users;
 
 import com.intuit.karate.junit5.Karate;
 
